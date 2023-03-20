1043c841fddbd330699e14436af716debdf134b6
Yulia Zinoveva <yulchik884@bk.ru>
2023-03-20 22:23:26 +0300

    first commit keysolo

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,14 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    this.currentSymbol = document.querySelector(".symbol_current");
+    document.addEventListener("keyup", (event) => {
+      if (event.key.toLowerCase() === this.currentSymbol.textContent.toLowerCase()) {
+        this.success()
+      } else {
+        this.fail();
+      }
+    })
   }
 
   success() {
