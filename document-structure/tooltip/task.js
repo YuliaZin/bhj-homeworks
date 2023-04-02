1f349b1038a2216b56cefe8e7a13da62c30d87d0
Yulia Zinoveva <yulchik884@bk.ru>
2023-04-02 17:24:17 +0300

    first commit tooltip

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,22 @@
+let arrayTooltiips = Array.from(document.querySelectorAll(".has-tooltip"));
+let tooltip = document.createElement("div");
+tooltip.setAttribute("data-position", "bottom");
+
+arrayOfTooltiips.forEach((item) => {
+    tooltip.className = "tooltip";
+
+    item.onclick = () => {
+        tooltip.className = "tooltip tooltip_active";
+        tooltip.textContent = item.title;
+        tooltip.style.top = item.getBoundingClientRect().top + item.getBoundingClientRect().height + "px";
+        tooltip.style.left = item.getBoundingClientRect().left + "px";
+
+        item.parentElement.insertBefore(tooltip, item);
+
+        return false
+    }
+
+    tooltip.onclick = () => {
+        tooltip.className = "tooltip";
+    }
+})
